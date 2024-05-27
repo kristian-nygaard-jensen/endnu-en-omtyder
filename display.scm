41207cae6ae3908d0dd3db55aebb2ecad87747a6
Kristian Nygaard Jensen <it@0071.se>
2024-05-28 00:13:57 +0200

    Koderåd

---
+++ display.scm
@@ -0,0 +1,5 @@
+(define (exs-exps)
+  (begin
+    (display "Hello World!")
+    (define name "Jane")
+    (* (- 8 (/ 30 5)) 21)));https://spritely.institute/static/papers/scheme-primer.html#scheme-conditionals-and-predicates#:~:text=Scheme%20has%20implicit%20return
