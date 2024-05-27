8eab7e9eafd4e816b01822f988fbca05b373ff1d
Kristian Nygaard Jensen <it@0071.se>
2024-05-28 00:29:02 +0200

    Backup

--- display.scm
+++ display.scm
@@ -2,4 +2,4 @@
   (begin
     (display "Hello World!")
     (define name "Jane")
-    (* (- 8 (/ 30 5)) 21)));https://spritely.institute/static/papers/scheme-primer.html#scheme-conditionals-and-predicates#:~:text=Scheme%20has%20implicit%20return
+    (* (- 8 (/ 30 5)) 21)));https://bafybeigi2gnukekr6ee72rkc3xlevzjxjabysguoddjsxmxgdxnv6aedqa.ipfs.w3s.link/#:~:text=Scheme%20has%20implicit%20return
